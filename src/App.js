377294497abb586a6718e31919e9cd712c8a874a
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-04-08 12:45:32 +0500

    update css of navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import About from './About';
-import './App.css';
-import Footer from './Footer';
-import Home from './Home';
-import Invetement from './Invetement';
+import React from "react";
+import About from "./About";
+import "./App.css";
+import Footer from "./Footer";
+import Home from "./Home";
+import Invetement from "./Invetement";
 import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
-      <Invetement />
-      <About />
-      <Footer />
+      <div className="main_page">
+        <Home />
+        <Invetement />
+        <About />
+        <Footer />
+      </div>
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -7,16 +7,10 @@ const Navbar = () => {
         <header>
         <div className="header_logo"><img src={logo} alt="logo_img" width="100" /></div>
         <div className="ul_links">
-          <ul>
-            <li>
               <a href=""><i className="fas fa-user-alt"></i><button><span>REFERRALS</span></button></a>
-            </li>
-            <li>
               <a href=""><i className="fas fa-question"></i><button><span>F.Q.A</span></button></a>
-            </li>
-          </ul>
         </div>
-        <div className="telegram_link"><ul><li><i className="fab fa-telegram-plane"></i></li></ul></div>
+        <div className="telegram_link"><i className="fab fa-telegram-plane"></i></div>
       </header>
         </div>
     );
