57029a6cac1782d48f20f19afc62b77bb19bd0dd
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-04-08 13:36:56 +0500

    completed home css

--- src/Home.js
+++ src/Home.js
@@ -6,8 +6,10 @@ const Home = () => {
   return (
     <div className="home_container">
       <section className="home_section">
-        <span>INVESTORS:0</span>
-        <span>TOTAL INVESTED:0 TRX</span>
+        <div className="home_main_span">
+          <span>INVESTORS:0</span>
+          <span>TOTAL INVESTED:0 TRX</span>
+        </div>
         <div className="home_section_left_right">
           <div className="home_section_left">
             <h3>100 % verified smart contract</h3>
@@ -19,18 +21,32 @@ const Home = () => {
                 </p>
               </div>
               <div className="percent_right">
-                <h1>15%</h1>
-                <h3>
+                <h1>
+                  <b>15%</b>
+                </h1>
+                <h2>
                   Daily
                   <br /> ROI
-                </h3>
+                </h2>
               </div>
             </div>
-            <p>GET 15% REFERRAL REWARDS</p>
-            <p>8% + 5% + 2%</p>
-            <div><a><button>CONTRACT</button></a><i className="fal fa-angle-right"></i></div>
+            <div className="home_p">
+              <p>GET 15% REFERRAL REWARDS</p>
+              <p>8% + 5% + 2%</p>
+            </div>
+            <div className="cotract_btn">
+              <a
+                href="https://tronscan.org/#/contract/TSXsR1ECTaGiMkJTAHSZ5yjz8Qx8FsLz7K/code"
+                target="_blank"
+              >
+                <button>CONTRACT</button>
+              </a>
+              <i className="fal fa-angle-right"></i>
+            </div>
           </div>
-          <div className="home_section_right"><img src={home_img} alt="homeimg" width="550" /></div>
+          <div className="home_section_right">
+            <img src={home_img} alt="homeimg" width="550" />
+          </div>
         </div>
       </section>
     </div>
