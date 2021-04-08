95f263c95d8a5b478d62e9c434b3aa2f830c0dd3
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-04-08 17:32:57 +0500

    about page css completed

--- src/About.js
+++ src/About.js
@@ -4,61 +4,89 @@ const About = () => {
   return (
     <div className="about_container">
       <section className="about_section">
-        <h1>HOW IT WORKS?</h1>
-        <p>
+        <h1 style={{ color: "white", textAlign: "center", fontSize: "40px" }}>
+          HOW IT WORKS?
+        </h1>
+        <p
+          style={{
+            color: "white",
+            textAlign: "center",
+            fontSize: "16px",
+            marginTop: "25px",
+          }}
+        >
           Tronstep is a decentralized investment platform powered by TRON smart
           contract technology. A smart contract guarantes 100 % safety and
           transparency of work.
         </p>
-        <p>
+        <p
+          style={{
+            color: "white",
+            textAlign: "center",
+            fontSize: "16px",
+            marginTop: "5px",
+          }}
+        >
           Accrual and payment of dividends occurs automatically. Tronstep is a
           great opportunity to multiply your assets in just a few days.
         </p>
-        <h2>4 ACTIONS WILL LEAD YOU TO SUCCESS</h2>
+        <h2
+          style={{
+            color: "#fcd740",
+            textAlign: "center",
+            fontSize: "30px",
+            marginTop: "50px",
+          }}
+        >
+          4 ACTIONS WILL LEAD YOU TO SUCCESS
+        </h2>
         <div className="action_parts">
           <div className="action_div">
             <div className="action_div_head">
-              <div className="action_color"></div>
-              <h3>REGISTER YOUR TRON ​​WALLET</h3>
-              <p>
-                To get started, you need to register a wallet that works with
-                the TRON (TRX) cryptocurrency. We recommend using TronLink or
-                TronWallet wallets to work with the Web site. In case you are
-                using a smartphone, the TronWallet app will be the best
-                solution!
-              </p>
-              <h2>1</h2>
+              <div className="action_color_1"></div>
+              <h4>REGISTER YOUR TRON ​​WALLET</h4>
             </div>
+            <p>
+              To get started, you need to register a wallet that works with the
+              TRON (TRX) cryptocurrency. We recommend using TronLink or
+              TronWallet wallets to work with the Web site. In case you are
+              using a smartphone, the TronWallet app will be the best solution!
+            </p>
+            <h2>1</h2>
           </div>
           <div className="action_div">
             <div className="action_div_head">
-              <div className="action_color"></div>
-              <h3>INVEST IN TRONSTEP</h3>
-              <p>
-              Smart contract Tronstep generates 20% of the investment amount every day and guarantees 200 % income in 14 days.
-              </p>
-              <h2>2</h2>
+              <div className="action_color_2"></div>
+              <h4>INVEST IN TRONSTEP</h4>
             </div>
+            <p>
+              Smart contract Tronstep generates 20% of the investment amount
+              every day and guarantees 200 % income in 14 days.
+            </p>
+            <h2>2</h2>
           </div>
           <div className="action_div">
             <div className="action_div_head">
-              <div className="action_color"></div>
-              <h3>RETURN ON INVESTMENT</h3>
-              <p>
-              Profit (dividends) is credited to your account immediately after making an investment. You can withdraw dividends at any time.
-              </p>
-              <h2>3</h2>
+              <div className="action_color_3"></div>
+              <h4>RETURN ON INVESTMENT</h4>
             </div>
+            <p>
+              Profit (dividends) is credited to your account immediately after
+              making an investment. You can withdraw dividends at any time.
+            </p>
+            <h2>3</h2>
           </div>
           <div className="action_div">
             <div className="action_div_head">
-              <div className="action_color"></div>
-              <h3>PROFIT FROM REFERRALS</h3>
-              <p>
-              Earn additional income by referring new users. A two-level referral program allows you to earn 10 % + 3 % + 2 % of the investment amount of invited investors.
-              </p>
-              <h2>4</h2>
+              <div className="action_color_4"></div>
+              <h4>PROFIT FROM REFERRALS</h4>
             </div>
+            <p>
+              Earn additional income by referring new users. A two-level
+              referral program allows you to earn 10 % + 3 % + 2 % of the
+              investment amount of invited investors.
+            </p>
+            <h2>4</h2>
           </div>
         </div>
       </section>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React from "react";
 import logo from "./logo.png";
 
 const Navbar = () => {
-    return (
-        <div className="navbar_container">
-        <header>
-        <div className="header_logo"><img src={logo} alt="logo_img" width="100" /></div>
+  return (
+    <div className="navbar_container">
+      <header>
+        <div className="header_logo">
+          <img src={logo} alt="logo_img" width="100" />
+        </div>
         <div className="ul_links">
-              <a href=""><i className="fas fa-user-alt"></i><button><span>REFERRALS</span></button></a>
-              <a href=""><i className="fas fa-question"></i><button><span>F.Q.A</span></button></a>
+
+          <a href="">
+            <i className="fas fa-user-alt"></i>
+            <div className="referals">
+              <button className="button">Refferals</button>
+            </div>
+          </a>
+
+          <a href="">
+            <i className="fas fa-question"></i>
+            <div className="referals">
+              <button>F.Q.A</button>
+            </div>
+          </a>
         </div>
-        <div className="telegram_link"><i className="fab fa-telegram-plane"></i></div>
+        <div className="telegram_link">
+          <i className="fab fa-telegram-plane"></i>
+        </div>
       </header>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Navbar;
